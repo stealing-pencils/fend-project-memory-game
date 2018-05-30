8867f224f0826d89c7015d648954a414e417036f
Katherine Riley <unoscarto@hotmail.com>
2018-05-29 21:11:25 -0700

    cards shuffle successfully

--- js/app.js
+++ js/app.js
@@ -4,6 +4,24 @@
  let singleCard = $('.card');
  let eachCard = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-anchor', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-diamond', 'fa fa-bomb', 'fa fa-leaf', 'fa fa-bomb', 'fa fa-bolt', 'fa fa-bicycle', 'fa fa-paper-plane-o', 'fa fa-cube'];
  let deck = $('.deck');
+
+ // Shuffle function from http://stackoverflow.com/a/2450976
+ function shuffle(array) {
+     var currentIndex = array.length, temporaryValue, randomIndex;
+
+     while (currentIndex !== 0) {
+         randomIndex = Math.floor(Math.random() * currentIndex);
+         currentIndex -= 1;
+         temporaryValue = array[currentIndex];
+         array[currentIndex] = array[randomIndex];
+         array[randomIndex] = temporaryValue;
+     }
+
+     return array;
+ }
+
+ eachCard = shuffle(eachCard);
+ console.log(eachCard);
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -13,36 +31,35 @@
 
 // *   - loop through each card and create its HTML
 // *   - add each card's HTML to the page
-for (card of eachCard) {
-	deck.append($('<li class="card"><i class="'
-   + eachCard + '"></i></li>'));
+for (let i = 0; i<eachCard.length; i++) {
+ deck.append($('<li class="card"><i class="' + eachCard[i] + '"></i></li>'));
 }
+
+// * set up the event listener for a card. If a card is clicked: */
+// *  - display the card's symbol (put this functionality in another function that you call from this one)
+function flipCardOver() {
+  $('.card').on("click", function(evt){
+   $(evt.target).toggleClass('open show');
+  });
+}
+
+flipCardOver();
+
+
 
 
 /*
- * set up the event listener for a card. If a card is clicked: */
-$('.card').on("click", function(evt){
-  $(evt.target).toggleClass('open show');
-});
+ // for (card of eachCard) {
+ //  console.log(card);
+ // 	deck.append($('<li class="card"><i class="'
+ //    + eachCard + '"></i></li>'));
+ // }
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
 
-    return array;
-}
 
-eachCard = shuffle(eachCard);
+
 /*
- *  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
