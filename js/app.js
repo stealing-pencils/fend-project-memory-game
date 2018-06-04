5095f4aa00be1861a0de65beff41eb0c30d62798
Katherine Riley <unoscarto@hotmail.com>
2018-06-04 08:42:17 -0700

    removed stars each time life is lost

--- js/app.js
+++ js/app.js
@@ -2,10 +2,12 @@
  * Create a list that holds all of your cards
  */
  let eachCard = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-anchor', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-diamond', 'fa fa-bomb', 'fa fa-leaf', 'fa fa-bomb', 'fa fa-bolt', 'fa fa-bicycle', 'fa fa-paper-plane-o', 'fa fa-cube'];
- let deck = $('.deck');
- let numberOfLives = 0;
+ let stars = $('.stars').find('.fa-star');
+ let numberOfLives = 3;
  let listOfCards = [];
  let lockCardsOpen = [];
+ let star = $('.score-panel ul li');
+
  // Shuffle function from http://stackoverflow.com/a/2450976
  function shuffle(array) {
      var currentIndex = array.length, temporaryValue, randomIndex;
@@ -23,17 +25,17 @@
 
  eachCard = shuffle(eachCard);
 /*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
 
  */
 
-
+//* Display the cards on the page
 // *   - loop through each card and create its HTML
 // *   - add each card's HTML to the page
 for (let i = 0; i<eachCard.length; i++) {
- deck.append($('<li class="card"><i class="' + eachCard[i] + '"></i></li>'));
+ $('.deck').append($('<li class="card"><i class="' + eachCard[i] + '"></i></li>'));
 }
+
+// $('.card').off("click");
 
 turnCardOver();
 // * set up the event listener for a card. If a card is clicked: */
@@ -41,49 +43,40 @@ function turnCardOver() {
   $('.card').on("click", function (evt){
     console.log(listOfCards);
     if ($(listOfCards).hasClass('show')){
-    // if ($(this).hasClass('show')){
-      console.log("got to part 2")
+      console.log("got to part 2");
       // *  - display the card's symbol (put this functionality in another function that you call from this one)
       showCard(this);
       cardMatch(listOfCards);
 
       } else {
         console.log("got to part 1");
-        // let cardOne = $('.open').html();
-        // let cardTwo = $(this).addClass('open show').html();
         showCard(this);
-        // listOfCards.push(this);
       }
 
   });
 }
 
-
+// *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
 function showCard(evt) {
+  // if (listOfCards.length <= 1) {
   $(evt).addClass('open show');
   console.log("got to showCard");
   addCardToList(evt);
-}
 
+
+  // } else {
+  //   $(".class").unbind("click");
+  // }
+}
+// add open card to the list of open cards
 function addCardToList(card) {
-  listOfCards.push(card);
   console.log('got to addCardList');
-  // turnCardOver();
+  listOfCards.push(card);
 }
-// function cardCollection(card) {
-//   listOfCards.push(card);
-//   if (listOfCards.length === 2 ) {
-//     console.log("got to cardCollection");
-//     cardMatch(listOfCards);
-//   }
-// }
 
-// *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
 // *  - if the list already has another card, check to see if the two cards match
-
 function cardMatch(listOfCards) {
   console.log('got to cardMatch');
-  // $('.open').html();
   let cardOne = $(listOfCards[0]).html();
   let cardTwo = $(listOfCards[1]).html();
   console.log(cardOne, cardTwo);
@@ -92,44 +85,39 @@ function cardMatch(listOfCards) {
     $('.open').addClass('match');
     keepCardsOpen(cardOne, cardTwo);
   } else {
-    // $('.open').removeClass('open show');
     console.log('it didnt work');
-    listOfCards.length = 0;
-    allFaceDown(listOfCards);
+    numberOfLives --;
+    allFaceDown();
   }
+}
 
+// *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
   function keepCardsOpen (cardOne, cardTwo) {
     console.log("got to keepCardsOpen function");
     lockCardsOpen.push(cardOne, cardTwo);
     console.log("These are locked cards: " + lockCardsOpen);
     listOfCards.length = 0;
     console.log("here is listOfCards after reset" + listOfCards);
-    // console.log(cardOne, cardTwo);
-  }
-  // if (listOfCards.length > 2) {
-  //   console.log("whooppee");
-  //   $('.open').addClass('match');
-  // } else {
-  //   console.log("nah");
-  //   setTimeout(function turnCardDown(){
-  //     $('.open').removeClass('open show');
-  //   }, 1500);
   }
 
-function allFaceDown(listOfCards) {
+  // *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+function allFaceDown() {
   setTimeout(function(){
     $('.open').removeClass('show open');
   }, 1500);
-  console.log(listOfCards);
+  moveCounter(numberOfLives);
+  listOfCards.length = 0;
 }
-// };
 
-
-
-
+function moveCounter(numberOfLives){
+  if (numberOfLives <= 3) {
+    star[numberOfLives].remove();
+   }
+    // $('.open').removeClass('show open');
+    console.log(numberOfLives);
+    // $('.li').removeClass('fa fa-star');
+}
 /*
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
